b9e93e24b1272f8225800845729cede1653e4730
yul100 <yulgc123@gmail.com>
2024-02-27 20:34:17 -0500

    nt3  feat  se programan condiciones del login

---
+++ main.py
@@ -0,0 +1,24 @@
+userDb="hidrotech123"
+passwordDB="admin123*"
+
+
+print('softwar tech  hidroituango')
+print('***************************')
+userName=input('digita tu usuario : ')
+userpassword = input("dijita tu contraseña: ")
+
+
+#evaluando las variables ingresada
+
+ 
+if userDb==userName  and passwordDB==userpassword:
+    print("exito en su usuario")
+else:
+    print("fallaste") 
+
+
+""" print("exito en su usuario")if  userDb==userName  and passwordDB==userpassword  else   print("fallaste") """#este el operador terniario
+
+
+
+
